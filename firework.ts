e12341e92aa81cc3276f14c1027b174e2dc84d47
takatofu <oyataka.8810.44@gmail.com>
2017-11-19 06:16:22 +0000

    Update firework.ts

--- firework.ts
+++ firework.ts
@@ -3,4 +3,8 @@ const magik = magikcraft.io;
 function firework() {
 	const there = magik.aspecto();
     magik.stella(there);
+    magik.stella(there);
+    magik.stella(there);
+    magik.stella(there);
+    magik.stella(there);
 }
